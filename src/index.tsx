d7787b55be2d1587e33216e6a6a7b953073be97d
asa0ryu-spec <asa0ryu@gmail.com>
2026-01-03 12:56:52 +0900

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -66,6 +66,15 @@ app.get('/', (c) => {
     <!DOCTYPE html>
     <html lang="ja">
     <head>
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-YP5WQNZ3Y8"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-YP5WQNZ3Y8');
+</script>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta name="author" content="Sabo Rikyu (Hidden Layer Works)">
